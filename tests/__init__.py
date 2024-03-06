1124471c8967337ed4ec7120bdbd48cfe6cb6135
Benjamin Pearce <gitlab@bcpearce.com>
2024-03-05 23:24:22 -0500

    test works with gtfs-station-stop 0.7.0b0

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for GTFS Realtime Custom Component."""
--- tests/test_init.py
+++ tests/test_init.py
@@ -3,10 +3,15 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
-from custom_components.gtfs_realtime.const import DOMAIN, URL_ENDPOINTS
+from custom_components.gtfs_realtime.const import (
+    API_KEY,
+    DOMAIN,
+    GTFS_STATIC_DATA,
+    URL_ENDPOINTS,
+)
 
 
 async def test_async_setup(hass: HomeAssistant) -> None:
     """Test the component gets setup."""
-    test_config = {DOMAIN: {URL_ENDPOINTS: []}}
+    test_config = {DOMAIN: {API_KEY: "", URL_ENDPOINTS: [], GTFS_STATIC_DATA: ""}}
     assert await async_setup_component(hass, DOMAIN, test_config) is True
